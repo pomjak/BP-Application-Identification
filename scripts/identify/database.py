576f63c208cd8215b6a3b17691db5ebcc89b2590
pomjak <pomjak71@gmail.com>
2024-11-16 19:25:50 +0100

    feat: first run fingerprints

--- scripts/identify/database.py
+++ scripts/identify/database.py
@@ -1,8 +1,32 @@
 """
 File: database.py
-Description: This file contains databases for storing fingerprints and frequent patterns.
+Description: This file contains databases for storing fingerprints.
 Author: Pomsar Jakub
 Xlogin: xpomsa00
 Created: 15/11/2024
 Updated: 16/11/2024
 """
+
+from logger import Logger
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+class Database:
+    def __init__(self, dataset):
+        self.dataset = dataset
+        with Logger() as logger:
+            logger.info("Parsing dataset ...")
+            self.df = pd.read_csv(self.dataset, delimiter=";")
+
+            # Split the dataset into training and testing data frames
+            self.train_df, self.test_df = train_test_split(self.df)
+            logger.info(
+                f"Split dataset {len(self.train_df)} training and {len(self.test_df)} testing samples"
+            )
+
+    def get_dataframe(self):
+        return self.df
+
+    def get_app(self, col, value):
+        return set(self.train_df[self.train_df[col] == value]["AppName"].tolist())
--- scripts/identify/identify.py
+++ scripts/identify/identify.py
@@ -7,8 +7,9 @@ Created: 15/11/2024
 Updated: 16/11/2024
 """
 
-from config import config
 from logger import Logger
+from config import config
+from database import Database
 from fingerprinting import JA3, JA4
 from pattern_matching import Apriori, SPADE, PrefixSpan
 
@@ -16,13 +17,36 @@ from pattern_matching import Apriori, SPADE, PrefixSpan
 def main():
     with Logger() as logger:
         logger.info("Starting identification process...")
-        dataset_path = config.ds
-        fingerprint_method = config.ja
-        pattern_algo = config.pattern_algo
 
-        print(f"Dataset path: {dataset_path}")
-        print(f"Fingerprinting method: {fingerprint_method}")
-        print(f"Pattern matching algorithm: {pattern_algo}")
+        db = Database(config.dataset)
+        ja3_correct = 0
+        ja3_incorrect = 0
+        ja4_correct = 0
+        ja4_incorrect = 0
+
+        for row in db.test_df.iterrows():
+            ja3hash = row[1]["JA3hash"]
+            appname = row[1]["AppName"]
+            ja4hash = row[1]["JA4hash"]
+
+            if appname in db.get_app("JA3hash", ja3hash):
+                ja3_correct += 1
+            else:
+                ja3_incorrect += 1
+
+            if appname in db.get_app("JA4hash", ja4hash):
+                ja4_correct += 1
+            else:
+                ja4_incorrect += 1
+
+        print(
+            f"JA3: Correct: {ja3_correct}, Incorrect: {ja3_incorrect}, Total: {ja3_correct + ja3_incorrect}"
+        )
+        print(
+            f"JA4: Correct: {ja4_correct}, Incorrect: {ja4_incorrect}, Total: {ja4_correct + ja4_incorrect}"
+        )
+        print(f"JA3: Accuracy: {ja3_correct / (ja3_correct + ja3_incorrect) * 100}%")
+        print(f"JA4: Accuracy: {ja4_correct / (ja4_correct + ja4_incorrect) * 100}%")
 
 
 if __name__ == "__main__":
