d00e222678188531fdbce4ff60ba9a4983ea940f
pomjak <pomjak71@gmail.com>
2024-11-17 17:01:14 +0100

    add: constants

---
+++ scripts/identify/constants.py
@@ -0,0 +1,15 @@
+"""
+File: constants.py
+Description: This file contains constants representing column names in the dataset.
+Author: Pomsar Jakub
+Xlogin: xpomsa00
+Created: 17/11/2024
+Updated: 17/11/2024
+"""
+
+# modify the constants to match the dataset column names
+JA3 = "JA3hash"
+JA3_S = "JA3Shash"
+JA4 = "JA4hash"
+JA4_S = "JA4Shash"
+APP_NAME = "AppName"
