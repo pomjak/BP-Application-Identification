4bd358bcdbf2d7e25261b6a7b5adba8be3c89a76
pomjak <pomjak71@gmail.com>
2025-03-12 09:34:55 +0100

    feat: usage of patterns stored separately

--- scripts/identify/pattern_matching.py
+++ scripts/identify/pattern_matching.py
@@ -4,7 +4,7 @@ Description: This file contains algorithms for detecting frequent patterns.
 Author: Pomsar Jakub
 Xlogin: xpomsa00
 Created: 15/11/2024
-Updated: 11/03/2025
+Updated: 12/03/2025
 """
 
 from prefixspan import prefixspan
@@ -30,7 +30,7 @@ class PatternMatchingMethod:
         self.third_guess = 0
 
         self.uniqueness = 0
-
+        self.usage_of_patterns = {}
         self.used_once_count = 0
         self.used_twice_count = 0
         self.used_more_times = 0
@@ -234,15 +234,13 @@ class Apriori(PatternMatchingMethod):
     def count_usage(self, db):
         for app in db.frequent_patterns:
             for file in db.frequent_patterns[app]:
-                set_of_patterns = db.frequent_patterns[app][file]
-
-                if self.is_set_marked_as_used(set_of_patterns, 1):
+                if self._get_usage_of_set(app, file, 1) == 1:
                     self.used_once_count += 1
 
-                elif self.is_set_marked_as_used(set_of_patterns, 2):
+                elif self._get_usage_of_set(app, file, 1) == 2:
                     self.used_twice_count += 1
 
-                elif self.is_set_marked_as_used(set_of_patterns, 0):
+                elif self._get_usage_of_set(app, file, 1) == 0:
                     self.used_never += 1
                 else:
                     self.used_more_times += 1
@@ -268,41 +266,40 @@ class Apriori(PatternMatchingMethod):
 
     def _update_correct_guess(self, guess_rank, set_of_patterns, similarity):
         if guess_rank == 1:
+            self._mark_set_as_used(similarity[1], similarity[2], 1)
             self.first_guess += 1
 
         elif guess_rank == 2:
             self.second_guess += 1
+            self._mark_set_as_used(similarity[1], similarity[2], 2)
 
         elif guess_rank == 3:
             self.third_guess += 1
+            self._mark_set_as_used(similarity[1], similarity[2], 3)
 
         self.correct += 1
-        self._mark_set_as_used(set_of_patterns[similarity[1]][similarity[2]])
 
     def _log_no_similar_apps_found(self):
         with Logger() as logger:
             logger.warn("No similar apps found")
 
-    def _mark_set_as_used(self, used_set):
+    def _mark_set_as_used(self, app, file, pos=1):
         """
         Mark set of patterns in training db as used for identifying the app correctly.
         """
-        if "used" not in used_set.columns:
-            used_set["used"] = 0
-        used_set["used"] += 1
 
-    # FIXME - ensure that it is working correctly
-    def is_set_marked_as_used(self, set_to_check, count=1):
-        if not isinstance(set_to_check, pd.DataFrame):
-            return False
+        if app not in self.usage_of_patterns:
+            self.usage_of_patterns[app] = {}
+        if file not in self.usage_of_patterns[app]:
+            self.usage_of_patterns[app][file] = [0, 0, 0]
 
-        if "used" not in set_to_check.columns and count == 0:
-            return True
+        self.usage_of_patterns[app][file][pos - 1] += 1
 
-        if "used" not in set_to_check.columns and count != 0:
-            return False
-
-        return (set_to_check["used"] == count).any()
+    def _get_usage_of_set(self, app, file, pos=1):
+        if app in self.usage_of_patterns and file in self.usage_of_patterns[app]:
+            return self.usage_of_patterns[app][file][pos - 1]
+        else:
+            return 0
 
     def get_number_of_unique_patterns_sets(self, trained_patterns, app=None):
         """
